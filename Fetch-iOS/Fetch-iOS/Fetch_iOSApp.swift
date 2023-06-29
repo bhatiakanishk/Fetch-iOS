1419b88c81caf72fd06b4b056d757120c79659b7
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-06-29 16:09:04 -0700

    initial commit

---
+++ Fetch-iOS/Fetch-iOS/Fetch_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOSApp.swift
+//  Fetch-iOS
+//
+//  Created by Kanishk Bhatia on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
