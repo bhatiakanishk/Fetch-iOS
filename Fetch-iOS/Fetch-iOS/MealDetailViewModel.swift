05ddf45565f33b35c1784bff2cacd839d9f81591
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-06-29 19:37:05 -0700

    Create MealDetailViewModel.swift

---
+++ Fetch-iOS/Fetch-iOS/MealDetailViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  MealDetailViewModel.swift
+//  Fetch-iOS
+//
+//  Created by Kanishk Bhatia on 6/29/23.
+//
+
+import Foundation
+import SwiftUI
+
+class MealDetailViewModel: ObservableObject {
+    @Published var mealDetail: MealDetail?
+    
+    func fetchMealDetail(mealId: String) {
+        guard let url = URL(string: "https://themealdb.com/api/json/v1/1/lookup.php?i=\(mealId)")
+        else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error fetching meal detail:  \(error.localizedDescription)")
+                return
+            }
+            if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    let response = try decoder.decode(MealDetailResponse.self, from: data)
+                    if let meal = response.meals.first {
+                        DispatchQueue.main.async {
+                            self.mealDetail = meal
+                        }
+                    }
+                } catch {
+                    print("Error decoding meal detail response: \(error.localizedDescription) ")
+                }
+            }
+        }.resume()
+    }
+}
+
+struct MealDetailResponse: Codable {
+    let meals: [MealDetail]
+}
